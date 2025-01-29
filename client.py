097d590ae21b095dd8aa65839a8f7ca38494aa57
mourad0208 <mou.eljalil@gmail.com>
2025-01-29 10:30:08 +0100

    client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import requests
 app = Flask(__name__)
 
 # URL de votre API
-API_URL = "http://127.0.0.1:8000"  # Remplacez par l'URL de votre API FastAPI
+API_URL = "http://35.180.242.250"  # Remplacez par l'URL de votre API FastAPI
 
 @app.route("/")
 def index():
